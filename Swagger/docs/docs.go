19e0e5f9c6d86b7b783bab11af2467276af31f27
AlozieAI <145882366+AlozieAI@users.noreply.github.com>
2024-10-05 18:21:30 -0400

    Update docs.go

--- Swagger/docs/docs.go
+++ Swagger/docs/docs.go
@@ -1210,7 +1210,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080  // This should be \"localhost\" instead of \"0.0.0.0\"",
+	Host:             "localhost:8080",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Swagger Example API",
