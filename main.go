2c611b77444ad1f24786d461cfe477d92fe8d79b
AlozieAI <145882366+AlozieAI@users.noreply.github.com>
2024-10-05 18:19:41 -0400

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8080  // This should be "localhost" instead of "0.0.0.0"
+// @host      localhost:8080 
 // @BasePath  /api/v1
 
 // @securityDefinitions.apikey bearerToken
